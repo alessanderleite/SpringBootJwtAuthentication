d63cba85364ebc18f3a169dc363f3c8c33e305e1
alessanderleite <alessander.logistica@gmail.com>
2019-07-07 14:53:38 -0300

    create model package and code the enum named RoleName

---
+++ src/main/java/br/com/alessanderleite/jwtauthentication/model/Role.java
@@ -0,0 +1,10 @@
+package br.com.alessanderleite.jwtauthentication.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	
+}
